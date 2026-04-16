d718cf3d2b00a7a6507195c69aa0aa9860703461
Devrim Ayyildiz <devrim.ayyildiz@canonical.com>
2026-04-16 10:46:49 +0200

    KOTO-655: Changes needed to get AI SDK snap work on V2L/G2L with Ubuntu Frame
    This commit includes the changes to get the rzv-ai-sdk-collection
    snap work on RZ/G2L with Ubuntu-Frame. So far, the applications:
    1. rzv-ai-sdk-collection.object-tracker
    2. rzv-ai-sdk-collection.object-counter
    3. rzv-ai-sdk-collection.object-detection
    
    are functional (without AI) with Ubuntu Frame.
    
    Note: all 3 applications are functional also with AI features on V2L
    
    Steps to test:
    $ sudo snap install --devmode ./rzv-ai-sdk-collection_1.0_arm64.snap
    $ ubuntu-frame &
    $ rzv-ai-sdk-collection.object-tracker MIPI
    $ rzv-ai-sdk-collection.object-counter animal MIPI
    $ rzv-ai-sdk-collection.object-detection

---
+++ launch-object-detection.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $SNAP/usr/r01/bin && exec \
+`LD_LIBRARY_PATH=$SNAP/usr/lib:$SNAP/usr/lib/aarch64-linux-gnu/:$SNAP/usr/lib/aarch64-linux-gnu/lapack:\
+$SNAP/usr/lib/aarch64-linux-gnu/blas:$SNAP/usr/lib/aarch64-linux-gnu/pulseaudio \
+XDG_RUNTIME_DIR=/run/user/1000 \
+WAYLAND_DISPLAY=wayland-0 \
+QT_QPA_PLATFORM=wayland-egl \
+QT_QPA_PLATFORM_PLUGIN_PATH=$SNAP/usr/lib/aarch64-linux-gnu/qt5/plugins \
+XKB_CONFIG_ROOT=$SNAP/usr/share/X11/xkb \
+__EGL_VENDOR_LIBRARY_DIRS=$SNAP/usr/share/glvnd/egl_vendor.d \
+./object_detection "$@"`
