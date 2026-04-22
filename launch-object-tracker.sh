b73fb5334478f0f3600e7261bcfaf1b4ebb043b2
Devrim Ayyildiz <devrim.ayyildiz@canonical.com>
2026-04-22 14:14:51 +0200

    Move environment variables from scripts to snapcraft.yaml
    Moved environment variables from scripts to snapcraft.yaml
    and created a separate part for the model used by object detection.

--- launch-object-tracker.sh
+++ launch-object-tracker.sh
@@ -5,13 +5,4 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-cd $SNAP/usr/q01/bin && exec \
-`LD_LIBRARY_PATH=$SNAP/usr/lib:$SNAP/usr/lib/aarch64-linux-gnu/:$SNAP/usr/lib/aarch64-linux-gnu/lapack:\
-$SNAP/usr/lib/aarch64-linux-gnu/blas:$SNAP/usr/lib/aarch64-linux-gnu/pulseaudio \
-XDG_RUNTIME_DIR=/run/user/1000 \
-WAYLAND_DISPLAY=wayland-0 \
-QT_QPA_PLATFORM=wayland-egl \
-QT_QPA_PLATFORM_PLUGIN_PATH=$SNAP/usr/lib/aarch64-linux-gnu/qt5/plugins \
-XKB_CONFIG_ROOT=$SNAP/usr/share/X11/xkb \
-__EGL_VENDOR_LIBRARY_DIRS=$SNAP/usr/share/glvnd/egl_vendor.d \
-./object_tracker "$@"`
+cd $SNAP/usr/q01/bin && exec ./object_tracker "$@"
